35e82c90198648ed54068986c0284554b4757a8a
Sahilkumar341 <sahilkumar26031@gmail.com>
2025-04-26 03:34:58 +0530

    change in render port

--- app.py
+++ app.py
@@ -163,5 +163,5 @@ def predict():
 # ----------------------------
 # Step 5: Run server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0',  port=int(os.environ.get("PORT", 5000)))
 
